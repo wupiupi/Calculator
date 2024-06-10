49efc86d8c5760e30fa76f42ecf4894970390928
Paul <pasha.makey@gmail.com>
2024-06-10 23:39:10 +0300

    CalculatorButton Enumeration added

--- Calculator/ContentView/ContentView.swift
+++ Calculator/ContentView/ContentView.swift
@@ -7,6 +7,50 @@
 
 import SwiftUI
 
+enum CalculatorButton: String, CaseIterable {
+    case zero = "0"
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case add = "+"
+    case subtract = "-"
+    case divide = "÷"
+    case mutliply = "x"
+    case equal = "="
+    case clear = "C"
+    case decimal = "."
+    case percent = "%"
+    case negative = "+/-"
+    
+    var buttonTitleColor: Color {
+        switch self {
+            case .percent, .divide, .mutliply, .subtract, .add:
+                return Color(hex: 0x66FF7F)
+            case .clear, .equal:
+                return Color(hex: 0x343434)
+            default:
+                return Color(hex: 0xFFFFFF)
+        }
+    }
+    
+    var buttonColor: Color {
+        switch self {
+            case .clear:
+                return Color(hex: 0xFF5959)
+            case .equal:
+                return Color(hex: 0x66FF7F)
+            default:
+                return Color(hex: 0x343434)
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         HStack {
