818a64c1963dcc23c40891a3bef5d34e0df567d5
Paul <pasha.makey@gmail.com>
2024-06-10 23:41:21 +0300

    Using Color Hex as Initializer for Color data type

--- Calculator/Extensions/Extension + Color.swift
+++ Calculator/Extensions/Extension + Color.swift
@@ -5,4 +5,17 @@
 //  Created by Paul Makey on 10.06.24.
 //
 
-import Foundation
+import SwiftUI
+
+/// - This Extension Allow to Use Color HEX Components as init for Color Type
+extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
