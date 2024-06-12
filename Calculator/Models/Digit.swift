08555d8bbce8bd8667adb7726e448f00b7000336
Paul <pasha.makey@gmail.com>
2024-06-12 12:59:09 +0300

    Enumeration with All Possible Digits

--- Calculator/Models/Digit.swift
+++ Calculator/Models/Digit.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+enum Digit: Int, CaseIterable, CustomStringConvertible {
+    case zero
+    case one
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    
+    var description: String {
+        "\(rawValue)"
+    }
+}
