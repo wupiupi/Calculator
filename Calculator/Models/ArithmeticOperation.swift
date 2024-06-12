86eaac8c272478f9a192fe98bffa1d39f200c475
Paul <pasha.makey@gmail.com>
2024-06-12 12:57:03 +0300

    Enumeration with Arithmetic Operations

--- Calculator/Models/ArithmeticOperation.swift
+++ Calculator/Models/ArithmeticOperation.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum ArithmeticOperation: CaseIterable, CustomStringConvertible {
+    case addition
+    case subtraction
+    case multiplication
+    case division
+    
+    var description: String {
+        switch self {
+        case .addition:
+            return "+"
+        case .subtraction:
+            return "−"
+        case .multiplication:
+            return "×"
+        case .division:
+            return "÷"
+        }
+    }
+}
