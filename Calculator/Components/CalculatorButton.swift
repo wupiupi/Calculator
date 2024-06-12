a55f84273e11fa4a8a6d2ad7f76f2feb305648ed
Paul <pasha.makey@gmail.com>
2024-06-12 17:56:17 +0300

    Refactoring

--- Calculator/Components/CalculatorButton.swift
+++ Calculator/Components/CalculatorButton.swift
@@ -7,42 +7,41 @@
 
 import SwiftUI
 
-struct CalculatorButton: View {
-    @EnvironmentObject private var viewModel: CalculatorViewModel
-    
-    let buttonType: ButtonType
-    
-    var body: some View {
-        Button {
-            viewModel.buttonDidTapped(buttonType)
-        } label: {
-            Text(buttonType.description)
-                .font(.system(size: 36))
-                .foregroundStyle(buttonType.foregroundColor)
-                .frame(
-                    width: buttonWidth(button: buttonType),
-                    height: buttonHeight()
-                )
-                .background {
-                    RoundedRectangle(cornerRadius: 20)
-                        .foregroundStyle(buttonType.backgroundColor)
-                }
+extension CalculatorView {
+    struct CalculatorButton: View {
+        @EnvironmentObject private var viewModel: CalculatorView.ViewModel
+        
+        let buttonType: ButtonType
+        
+        var body: some View {
+            Button {
+                viewModel.buttonDidTapped(buttonType)
+            } label: {
+                Text(buttonType.description)
+                    .font(.system(size: 36))
+                    .foregroundStyle(buttonType.foregroundColor)
+                    .frame(
+                        width: getButtonSize(buttonType),
+                        height: buttonHeight()
+                    )
+                    .background {
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundStyle(buttonType.backgroundColor)
+                    }
+            }
         }
-    }
-    
-    // Calculating Button Size Depending on the Screen Size
-    private func buttonWidth(button: ButtonType) -> CGFloat {
-        /// Logic behind the calculations:
-        /// - UIScreen.main.bounds.width – Taking width of the Screen
-        /// - (4 * 12) – Subtract the Padding of the Each Column
-        /// - / 4 – Dividing by the Columns We Have
-        if button == .equal {
-            return ((UIScreen.main.bounds.width - (4 * 12)) / 4) * 2
+        
+        // Calculating Button Size Depending on the Screen Size
+        private func getButtonSize(_ button: ButtonType) -> CGFloat {
+            /// buttonSize = (screenWidth - (spacingCount * padding)) / buttonCount
+            if button == .equal {
+                return ((UIScreen.main.bounds.width - (3 * 12)) / 4) * 2
+            }
+            return (UIScreen.main.bounds.width - (5 * 12)) / 4
         }
-        return (UIScreen.main.bounds.width - (5 * 12)) / 4
-    }
-    
-    private func buttonHeight() -> CGFloat {
-        return (UIScreen.main.bounds.width - (5 * 12)) / 4
+        
+        private func buttonHeight() -> CGFloat {
+            return (UIScreen.main.bounds.width - (5 * 12)) / 4
+        }
     }
 }
